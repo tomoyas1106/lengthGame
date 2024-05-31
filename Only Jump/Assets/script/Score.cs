a60f22cd0756c4bd5f422bf5613800a6985bdc78
Tomoya1106 <Tomoyayamasita1106@gmail.com>
2024-05-31 15:46:12 +0900

    [fix]UI周りとプレイヤーのマテリアル変更

--- Only Jump/Assets/script/Score.cs
+++ Only Jump/Assets/script/Score.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class Score : MonoBehaviour
 {
-    public  float _score = 0f;
     public  Text text;
+
 
     void Update()
     {
-       _score += Time.deltaTime;
+        ShowRate();
     }
     // Update is called once per frame
     public void ShowRate()
     {
-        float score = _score;
 
-        text.text = $"{score:P0}";
+        text.text = $"{ScoreMaker._score:P0}";
     }
 }
